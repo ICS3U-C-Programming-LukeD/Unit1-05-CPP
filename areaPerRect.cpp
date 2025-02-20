2ce4b6f73bf48a59e6bb1be835304a2763cfe13d
Luke Di Bert <luke.di-bert@stu.ocsb.ca>
2025-02-20 04:55:16 +0000

    added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Luke Di Bert All rights reserved.
+//
+// Created by: Luke Di Bert
+// Date: Feb 19, 2025
+// Calculates the area and perimeter of a rectangle
+
+#include <cmath>
+#include <iostream>
+int main() {
+    std::cout << "A rectangle with the" << std::endl;
+    std::cout << "width of 10 and length of 6" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "The area is " << 6 * 10 << std::endl;
+    std::cout << "The perimeter is " << 2 * (10 + 6) << std::endl;
+}
